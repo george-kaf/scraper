d45e0d99e43d5f3731128cf855ef846155a5fb48
george-kaf <106911993+george-kaf@users.noreply.github.com>
2024-03-21 01:57:50 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,33 +2,42 @@ from bs4 import BeautifulSoup
 import re
 from IPython.display import HTML
 import requests
+from pprint import pprint
 
 def scraper(url):
+    # Remove duplicate links from file
+    def remove_duplicates():
+        lines = open('scraped_links.txt', 'r')
+        lines_set = set(lines.readlines())
+        out  = open('scraped_links.txt', 'w')
+        for line in lines_set:
+            out.write(line)
+        lines.close()
+        out.close()
+
     html = requests.get(url)
     soup = BeautifulSoup(html.content, 'html.parser')
 
-    # Create a file with the allergens
-    file = open('scraped_data.txt', 'w')
-    file.write(soup.prettify())
-    file.flush()
-    file.close()
-    
+    # Write HTML to file
+    with open('scraped_data.txt', 'w') as file:
+        file.write(soup.prettify())
+
     # Create a .txt file with all the links of the scraped page
     # For a tags with href add , attrs={'href': re.compile('^https')} to find_all()
-    file = open('scraped_links.txt', 'w')
-    for link in soup.find_all('a'):
-        soup_link = str(link.get('href'))
-        # convert relative links to base url + link
-        if 'punkcake.rocks' and 'https' not in soup_link:
-            soup_link = 'punkcake.rocks' + soup_link
-            file.write(soup_link + '\n')
-        else:
-            file.write(soup_link + '\n')
-            print(soup_link)
+    with open('scraped_links.txt', 'w+') as file:
+        for link in soup.find_all('a'):
+            soup_link = str(link.get('href'))
 
-    file.flush()
-    file.close()
-    
+            # remove any id links
+            if '#' not in soup_link:
+                # convert relative links to base url + link
+                if 'punkcake.rocks' and 'https' not in soup_link:
+                    soup_link = 'punkcake.rocks' + soup_link
+                    file.write(soup_link + ', \n')
+                else:
+                    file.write(soup_link + ', \n')
+           
+    # Create a file with the allergens
     try:
         #Get title from product page
         title_html = soup.body.findAll('h1', {'class': 'h2 product-single__title'})
@@ -46,6 +55,7 @@ def scraper(url):
         file.close()
     except:
         pass
-    
+
+    remove_duplicates()
 
 scraper('https://punkcake.rocks')
