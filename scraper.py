aeb1f47000e7e1ffb0fbc54da22527fd512edb97
george-kaf <giorgoskafetzo@gmail.com>
2024-03-22 00:26:53 +0000

    first_commit

--- scraper.py
+++ scraper.py
@@ -1,59 +1,62 @@
-from bs4 import BeautifulSoup
 import re
+
 import requests
+from bs4 import BeautifulSoup
+
 
 def scraper(url, base_url):
     # Remove duplicate links from file
     def remove_duplicates():
-        lines = open('scraped_links.txt', 'r')
+        lines = open("scraped_links.txt", "r")
         lines_set = set(lines.readlines())
-        out  = open('scraped_links.txt', 'w')
+        out = open("scraped_links.txt", "w")
         for line in lines_set:
             out.write(line)
         lines.close()
         out.close()
 
     html = requests.get(url)
-    soup = BeautifulSoup(html.content, 'html.parser')
+    soup = BeautifulSoup(html.content, "html.parser")
 
     # Write HTML to file
-    with open('scraped_data.txt', 'w') as file:
+    with open("scraped_data.txt", "w") as file:
         file.write(soup.prettify())
 
     # Create a .txt file with all the links of the scraped page
     # For a tags with href add , attrs={'href': re.compile('^https')} to find_all()
-    with open('scraped_links.txt', 'w+') as file:
-        for link in soup.find_all('a'):
-            soup_link = str(link.get('href'))
+    with open("scraped_links.txt", "w+") as file:
+        for link in soup.find_all("a"):
+            soup_link = str(link.get("href"))
 
             # remove any id links
-            if '#' not in soup_link:
+            if "#" not in soup_link:
                 # convert relative links to base url + link
-                if base_url and 'http' not in soup_link:
+                if base_url and "http" not in soup_link:
                     soup_link = str(base_url) + soup_link
-                    file.write(soup_link + ', \n')
+                    file.write(soup_link + ", \n")
                 else:
-                    file.write(soup_link + ', \n')
-           
+                    file.write(soup_link + ", \n")
+
     # Create a file with the allergens
     try:
-        #Get title from product page
-        title_html = soup.body.findAll('h1', {'class': 'h2 product-single__title'})
+        # Get title from product page
+        title_html = soup.body.findAll("h1", {"class": "h2 product-single__title"})
         title = re.findall('">[a-zA-Z]+.+.?', str(title_html))[0].split('">')[1].capitalize()
-        
-        #Get allergen info from description and filter the allergens
-        allergen_html = soup.body.findAll('div', {'class': 'rte'})
-        allergens = re.findall('Allergens:.[a-zA-Z]+.+.?', str(allergen_html))[0].split("Allergens:")
+
+        # Get allergen info from description and filter the allergens
+        allergen_html = soup.body.findAll("div", {"class": "rte"})
+        allergens = re.findall("Allergens:.[a-zA-Z]+.+.?", str(allergen_html))[0].split("Allergens:")
         allergens = allergens[1].split("</span>")[0].capitalize()
 
         # Create a file with the allergens
-        file = open('allergen_data.txt', 'w')
-        file.write(title + ' - ' + allergens)
+        file = open("allergen_data.txt", "w")
+        file.write(title + " - " + allergens)
         file.flush()
         file.close()
     except:
-        print('No products in current page.')
+        print("No products in current page.")
 
     remove_duplicates()
 
-scraper('https://punkcake.rocks', 'punkcake.rocks')
+
+scraper("https://punkcake.rocks", "punkcake.rocks")
